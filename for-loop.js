8ed8a0e39af768679977aac7846a9c5f6938369f
atashazachrine-user <atashazacharine@gmail.com>
2025-11-20 08:35:57 +0800

    Added for-loop

---
+++ for-loop.js
@@ -0,0 +1,20 @@
+//for loop
+
+//for (i = 1; i <= 5; i++) {
+//    output += i + " ";
+//}
+//console.log(output);
+
+
+//Arrays
+
+output = "";
+arr = [1, 2, 3]
+console.log("Array length: " + arr.length  );
+
+for (index = arr .length - 1; index >= 0; index++) {
+//console .log (arr [index]) ;
+    output += arr[index] + " ";
+}
+
+console.log(output);
