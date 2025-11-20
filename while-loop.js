f01c2f94051b7a51b8631cc1bae3e53e97badf84
atashazachrine-user <atashazacharine@gmail.com>
2025-11-20 08:33:35 +0800

    Added while-loop

---
+++ while-loop.js
@@ -0,0 +1,41 @@
+/*
+i = 1;
+
+while (i <= 1) {
+    console.log(i);
+    i--;
+}
+*/
+
+/*
+console.log("Ascending");
+x = 1;
+
+while (i <= 5) {
+    console.log(x);
+    x++;
+}
+*/
+
+/*
+output = "";
+x=1;
+
+while (x <= 5) {
+    output += x + " ";
+    x++;
+}
+
+console.log(output);
+*/
+
+arr = [1, 2, 3];
+output = "";
+index = arr.length - 1;
+
+while (index <= arr.lenght-1) {
+    output += arr[index] + " ";
+    index--;
+}
+
+console.log(output);
