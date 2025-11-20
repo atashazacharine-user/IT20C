2ffb36d5d16b1614eced07fa552baf8eea6b6f99
atashazachrine-user <atashazacharine@gmail.com>
2025-11-20 09:01:10 +0800

    Added do-while-loop

--- do-while-loop.js
+++ do-while-loop.js
@@ -8,7 +8,7 @@ do{
 }while(i<=5)
 
 */
-
+/*
 output = "";
 i = 1;
 
@@ -16,5 +16,17 @@ do {
     output += i;
     i++;
 } while (i <= 5);
+
+console.log(output);
+*/
+
+arr = [4, 2, 3];
+index = 0;
+output = "";
+
+do{
+    output += arr[index] + " ";
+    index++;
+} while(index < arr.length);
 
 console.log(output);
